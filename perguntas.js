d1dcf55af09e4fe803a4fc1c2a7573786f8cb9a4
Yuritom456 <yuritomassuncao567@gmail.com>
2024-10-28 14:42:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'SCP',
+    'O que é SCP',
+    'Os scp são criaturas e objetos anormais'
+)
+
+criaCartao(
+    'SCP',
+    'O que significa SCP',
+    'Seguro,Conter,Proteger'
+)
+
+criaCartao(
+    'SCP',
+    'Qual são os rankings de scp',
+    'Safe,Euclid,Keter'
+)
+
+criaCartao(
+    'SCP',
+    'O que é Safe',
+    'Os Safe são os SCP mais fácil de serem contidos e os mais seguros'
+)
+
+criaCartao(
+    'SCP',
+    'O que é Euclid',
+    'Os Euclid são os SCP mais mediano de serem contidos'
+)
+
+criaCartao(
+    'SCP',
+    'O que é Keter',
+    'Os Keter são os SCP mais difícil de serem contidos e os mais perifoss'
+)
